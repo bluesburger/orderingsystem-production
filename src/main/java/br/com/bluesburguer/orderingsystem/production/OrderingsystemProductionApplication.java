a7ea6ae52d339e687c3c2673726b6d66080ef352
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-04-28 23:23:05 -0300

    Correção de apresentação de métricas via actuator

--- src/main/java/br/com/bluesburguer/orderingsystem/production/OrderingsystemProductionApplication.java
+++ src/main/java/br/com/bluesburguer/orderingsystem/production/OrderingsystemProductionApplication.java
@@ -1,23 +1,18 @@
 package br.com.bluesburguer.orderingsystem.production;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.stereotype.Controller;
 
 /** 
  * 
 */
 @SpringBootApplication
-@Controller
 @EnableDiscoveryClient
 public class OrderingsystemProductionApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(OrderingsystemProductionApplication.class, args);
-		new SpringApplicationBuilder(OrderingsystemProductionApplication.class)
-			// .web(WebApplicationType.SERVLET)
-			.run(args);
+		SpringApplication.run(OrderingsystemProductionApplication.class, args);
 	}
 
 }
