4f5863832159487750b7407e5473e840d2d80564
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-04-29 13:29:55 -0300

    Correção de profiles

--- src/test/java/br/com/bluesburguer/orderingsystem/production/utils/BaseIntegrationTest.java
+++ src/test/java/br/com/bluesburguer/orderingsystem/production/utils/BaseIntegrationTest.java
@@ -1,10 +1,12 @@
 package br.com.bluesburguer.orderingsystem.production.utils;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource("classpath:application-test.properties")
+@ActiveProfiles({ "test" })
 public class BaseIntegrationTest {
 
 }
