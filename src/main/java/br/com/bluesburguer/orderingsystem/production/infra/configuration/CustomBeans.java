612ab612b79177e02abf617dc6fb257055e57d23
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-04-30 13:54:44 -0300

    Melhoorias para database migration e SQS

--- src/main/java/br/com/bluesburguer/orderingsystem/production/infra/configuration/CustomBeans.java
+++ src/main/java/br/com/bluesburguer/orderingsystem/production/infra/configuration/CustomBeans.java
@@ -33,7 +33,7 @@ public class CustomBeans {
 	                CorsEndpointProperties corsProperties, 
 	                WebEndpointProperties webEndpointProperties, 
 	                Environment environment) {
-	    List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
+	    List<ExposableEndpoint<?>> allEndpoints = new ArrayList<>();
 	    Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
 	    allEndpoints.addAll(webEndpoints);
 	    allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
@@ -45,7 +45,7 @@ public class CustomBeans {
 	    return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, 
 	                endpointMediaTypes, corsProperties.toCorsConfiguration(), 
 	                new EndpointLinksResolver(allEndpoints, basePath), 
-	                shouldRegisterLinksMapping, null);
+	                shouldRegisterLinksMapping);
 	}
 
 	private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, 
