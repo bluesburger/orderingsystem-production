0560eacd0a7f79ed9d0e478d61b6fc1b0289f2cc
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-05-03 20:13:13 -0300

    Melhorias para uso de dead-letter queue em todas as filas FIFO

--- Makefile
+++ Makefile
@@ -1,68 +1,12 @@
-install: up create-queues defining-dead-letter-queue-rule starting-message-move-task list-queues
+install: down up
 
 up:
 	@ echo Up service
-	@ docker compose up -d --build
-
-create-queues:
-	@ echo Creating queues
-	@ docker compose exec localstack awslocal sqs create-queue --queue-name localstack-queue-order-paid.fifo --attributes "FifoQueue=true" \
-		--output text --color auto
-	@ docker compose exec localstack awslocal sqs create-queue --queue-name localstack-queue-order-in-production.fifo --attributes "FifoQueue=true" \
-		--output text --color auto
-	@ docker compose exec localstack awslocal sqs create-queue --queue-name localstack-queue-order-produced.fifo --attributes "FifoQueue=true" \
-		--output text --color auto
-	@ docker compose exec localstack awslocal sqs create-queue --queue-name localstack-queue-order-delivering.fifo --attributes "FifoQueue=true" \
-		--output text --color auto
-	@ docker compose exec localstack awslocal sqs create-queue --queue-name localstack-queue-order-delivered.fifo --attributes "FifoQueue=true" \
-		--output text --color auto
-	@ docker compose exec localstack awslocal sqs create-queue --queue-name localstack-queue-order-canceled.fifo --attributes "FifoQueue=true" \
-		--output text --color auto
- 
-	@ docker compose exec localstack awslocal sqs create-queue --queue-name localstack-queue-order-paid-dead-letter \
-		--output text --color auto
-	@ docker compose exec localstack awslocal sqs create-queue --queue-name localstack-queue-order-paid-recovery \
-		--output text --color auto
-		
-	@ echo Creating test queues
-	@ docker compose exec localstack awslocal sqs create-queue --queue-name localstack-queue.fifo --attributes "FifoQueue=true" \
-		--output text --color auto
-	@ docker compose exec localstack awslocal sqs create-queue --queue-name localstack-queue-dead-letter \
-		--output text --color auto
-	@ docker compose exec localstack awslocal sqs create-queue --queue-name localstack-queue-recovery \
-		--output text --color auto
-	@ docker compose exec localstack awslocal sqs create-queue --queue-name queuemessage.fifo --attributes "FifoQueue=true" --output text --color auto	
-
-defining-dead-letter-queue-rule:
-	@ echo Defining Dead Letter Queue Attributes
-	@ docker compose exec localstack awslocal sqs set-queue-attributes \
-		--queue-url http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/localstack-queue-order-paid.fifo \
-		--attributes '{ "RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:us-east-1:000000000000:localstack-queue-order-paid-dead-letter\",\"maxReceiveCount\":\"1\"}" }' \
-		--output text
-		
-	@ echo Defining test Dead Letter Queue Attributes
-	@ docker compose exec localstack awslocal sqs set-queue-attributes \
-		--queue-url http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/localstack-queue.fifo \
-		--attributes '{ "RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:us-east-1:000000000000:localstack-queue-dead-letter\",\"maxReceiveCount\":\"1\"}" }' \
-		--output text
-
-starting-message-move-task:
-	@ echo Starting Message Move Task
-	@ docker compose exec localstack awslocal sqs start-message-move-task \
-        --source-arn arn:aws:sqs:us-east-1:000000000000:localstack-queue-order-paid-dead-letter \
-        --destination-arn arn:aws:sqs:us-east-1:000000000000:localstack-queue-order-paid-recovery \
-		--output text
-		
-	@ echo Starting Test Message Move Task
-	@ docker compose exec localstack awslocal sqs start-message-move-task \
-        --source-arn arn:aws:sqs:us-east-1:000000000000:localstack-queue-dead-letter \
-        --destination-arn arn:aws:sqs:us-east-1:000000000000:localstack-queue-recovery \
-		--output text
-
-list-queues:
-	@ echo Showing existant queues
-	@ docker compose exec localstack awslocal sqs list-queues \
-		--output text
+	@ docker compose up -d --build	
+	
+down:
+	@ echo Down services
+	@ docker compose down
 
 show-order-paid-queue:
 	@ docker compose exec localstack awslocal sqs get-queue-attributes --queue-url http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/localstack-queue-order-paid.fifo --attribute-names All
@@ -71,8 +15,4 @@ show-test-queue:
 	@ docker compose exec localstack awslocal sqs get-queue-attributes --queue-url http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/localstack-queue.fifo --attribute-names All
 
 show-test-message-queue:
-	@ docker compose exec localstack awslocal sqs get-queue-attributes --queue-url http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/queuemessage.fifo --attribute-names ApproximateNumberOfMessages	
-	
-down:
-	@ echo Down services
-	@ docker compose down
+	@ docker compose exec localstack awslocal sqs get-queue-attributes --queue-url http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/queuemessage.fifo --attribute-names ApproximateNumberOfMessages
--- src/main/java/br/com/bluesburguer/orderingsystem/production/infra/SqsQueueBean.java
+++ src/main/java/br/com/bluesburguer/orderingsystem/production/infra/SqsQueueBean.java
@@ -1,12 +1,20 @@
 package br.com.bluesburguer.orderingsystem.production.infra;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.model.AmazonSQSException;
 import com.amazonaws.services.sqs.model.CreateQueueRequest;
 import com.amazonaws.services.sqs.model.CreateQueueResult;
+import com.amazonaws.services.sqs.model.GetQueueAttributesRequest;
+import com.amazonaws.services.sqs.model.QueueAttributeName;
+import com.amazonaws.services.sqs.model.SetQueueAttributesRequest;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,34 +23,98 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @RequiredArgsConstructor
 public class SqsQueueBean implements InitializingBean {
-	
+
 	private final AmazonSQSAsync amazonSQSAsyncClient;
+	
+	@Value("${aws.sqs.visibility.timeout:60}")
+	private String visibilityTimeout;
+	
+	@Value("${aws.sqs.delay.seconds:10}")
+	private String delaySeconds;
+	
+	@Value("${aws.sqs.message.retention.period:86400}")
+	private String messageRetentionPeriod;
+
+	public static final List<String> FIFO_QUEUES = List.of(
+			"SQS_DEMO_QUEUE", 
+			"order-paid-queue",
+			"order-in-production-queue", 
+			"order-produced-queue",
+			"order-delivering-queue", 
+			"order-delivered-queue",
+			"order-canceled-queue");
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		log.info("Queue created: {}", createFifoQueue().getQueueUrl());
-		
+		createFifoQueues();
 	}
 
-	public CreateQueueResult createQueue(String name) {
-        CreateQueueRequest standardQueueRequest = new CreateQueueRequest()
-                .withQueueName("SQS_DEMO_QUEUE")
-                .addAttributesEntry("VisibilityTimeout", "60")
-                .addAttributesEntry("DelaySeconds", "10")
-                .addAttributesEntry("MessageRetentionPeriod", "86400");
+	void createFifoQueues() {
+		FIFO_QUEUES.stream().forEach(originalQueueName -> {
+			var createdQueue = createFifoQueue(originalQueueName);
+			var deadLetterQueueUrl = createFifoDeadLetterQueue(originalQueueName);
+			log.info("Queue created: {} with associated Dead Letter: {}", createdQueue.getQueueUrl(),
+					deadLetterQueueUrl);
+		});
+	}
 
-        return amazonSQSAsyncClient.createQueue(standardQueueRequest);
-    }
-    
-    public CreateQueueResult createFifoQueue() throws AmazonSQSException {
-		CreateQueueRequest fifoQueueRequest = new CreateQueueRequest()
-                .withQueueName("SQS_DEMO_QUEUE.fifo")
-                .addAttributesEntry("FifoQueue", "true")
-                .addAttributesEntry("VisibilityTimeout", "60")
-                .addAttributesEntry("DelaySeconds", "10")
-                .addAttributesEntry("ContentBasedDeduplication", "true")
-                .addAttributesEntry("MessageRetentionPeriod", "86400");
-		
+	CreateQueueResult createQueue(String name) {
+		var standardQueueRequest = new CreateQueueRequest()
+				.withQueueName(name)
+				.addAttributesEntry("VisibilityTimeout", visibilityTimeout)
+				.addAttributesEntry("DelaySeconds", delaySeconds)
+				.addAttributesEntry("MessageRetentionPeriod", messageRetentionPeriod);
+
+		return amazonSQSAsyncClient.createQueue(standardQueueRequest);
+	}
+
+	CreateQueueResult createFifoQueue(String newQueueName) throws AmazonSQSException {
+		var queueName = normalizeFifoQueueName(newQueueName);
+		var fifoQueueRequest = new CreateQueueRequest()
+				.withQueueName(queueName)
+				.addAttributesEntry("FifoQueue", "true")
+				.addAttributesEntry("VisibilityTimeout", visibilityTimeout)
+				.addAttributesEntry("DelaySeconds", delaySeconds)
+				.addAttributesEntry("MessageRetentionPeriod", messageRetentionPeriod)
+				.addAttributesEntry("ContentBasedDeduplication", "true");
+
 		return amazonSQSAsyncClient.createQueue(fifoQueueRequest);
-    }
+	}
+
+	String createFifoDeadLetterQueue(String baseQueueName) throws AmazonSQSException {
+		var newQueueName = String.format("%s-dead-letter.fifo", baseQueueName);
+
+		// Criar queue
+		var createDeadLetterQueueRequest = new CreateQueueRequest()
+				.addAttributesEntry("FifoQueue", "true")
+				.withQueueName(newQueueName);
+		var createdQueueResult = amazonSQSAsyncClient.createQueue(createDeadLetterQueueRequest);
+
+		// Obter ARN da queue
+		var deadLetterQueueUrl = createdQueueResult.getQueueUrl();
+		var attributeRequest = new GetQueueAttributesRequest().withQueueUrl(deadLetterQueueUrl)
+				.withAttributeNames(QueueAttributeName.QueueArn);
+
+		var deadLetterQueueAttributes = amazonSQSAsyncClient.getQueueAttributes(attributeRequest);
+		var deadLetterQueueARN = deadLetterQueueAttributes.getAttributes().get(QueueAttributeName.QueueArn.name());
+
+		// Definir a fila como DEAD LETTER da fila criada
+		Map<String, String> attributes = new HashMap<>();
+		attributes.put(QueueAttributeName.RedrivePolicy.name(),
+				"{\"maxReceiveCount\":\"5\", \"deadLetterTargetArn\":\"" + deadLetterQueueARN + "\"}");
+
+		var queueAttributeRequest = new SetQueueAttributesRequest()
+				.withQueueUrl(normalizeFifoQueueName(baseQueueName))
+				.withAttributes(attributes);
+
+		amazonSQSAsyncClient.setQueueAttributes(queueAttributeRequest);
+
+		return deadLetterQueueUrl;
+	}
+	
+	String normalizeFifoQueueName(String queueName) {
+		return queueName.toLowerCase().endsWith(".fifo") 
+				? queueName 
+				: queueName + ".fifo";
+	}
 }
