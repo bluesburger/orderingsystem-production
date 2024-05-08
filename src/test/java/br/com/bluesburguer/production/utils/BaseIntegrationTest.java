adc7a2f657ea1c00e682ccb7413d126a5b252321
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-05-08 19:30:15 -0300

    Corrigido teste de integração principal

--- src/test/java/br/com/bluesburguer/production/utils/BaseIntegrationTest.java
+++ src/test/java/br/com/bluesburguer/production/utils/BaseIntegrationTest.java
@@ -4,8 +4,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+import br.com.bluesburguer.production.OrderingsystemProductionApplication;
+
 @TestPropertySource("classpath:application-test.properties")
+@SpringBootTest(
+		classes = { OrderingsystemProductionApplication.class },
+		properties = { "spring.main.allow-bean-definition-overriding=true" },
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 @ActiveProfiles({ "test" })
 public class BaseIntegrationTest {
 
